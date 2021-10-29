0cac96a8865d487ddba5a0448fa23b1d9c8a8eaf
imranjansharma <91980705+imranjansharma@users.noreply.github.com>
2021-10-29 18:45:24 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
-class Simple{  
+//declaring a class
+class Simple{ 
+    //java main() mehtod
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     //System.out.println() method to print the passed string in console window
+     System.out.println("Hello World");  
     }  
 }  
